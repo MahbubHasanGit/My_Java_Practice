31742fb9020c26a1f029b4bdb8f57edf8230263c
MahbubHasanGit <45738314+MahbubHasanGit@users.noreply.github.com>
2019-03-26 16:37:44 +0600

    How to declare variable and print variable value..

---
+++ Variable_Declaration.java
@@ -0,0 +1,22 @@
+package my_java;
+
+/**
+ *
+ * @author Mahbub Hasan
+ */
+public class Variable_Declaration {
+    public static void main(String[] args) {
+   int int_number = 10 ;
+   float float_number = 10.50f ;
+   double double_number = 10.12354 ;
+   char character = 'm' ;
+   String string = "Mahbub Hasan" ;
+   
+   System.out.println("The integer number is "+int_number);
+   System.out.println("The float number is "+float_number);
+   System.out.println("The double number is "+double_number);
+   System.out.println("The character is "+character);
+   System.out.println("The string is "+string);
+}
+    
+}
