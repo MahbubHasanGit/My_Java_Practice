c005246bb392c7287b24b082a68ec496308003bb
MahbubHasanGit <45738314+MahbubHasanGit@users.noreply.github.com>
2019-03-26 16:26:44 +0600

    Add files via upload

---
+++ Basis_Structure.java
@@ -0,0 +1,13 @@
+
+package my_java;
+
+/**
+ *
+ * @author Mahbub Hasan
+ */
+public class Basis_Structure {
+    public static void main(String[] args) {
+   System.out.println("This is the Basic Structure of Java Programming..");
+}
+    
+}
